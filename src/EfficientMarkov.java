7dc2ffc7ccd31568e6f3ba284c596174b835d616
Jenny Huang <jenny.huang@duke.edu>
2018-10-22 14:19:15 -0400

    ok

---
+++ src/EfficientMarkov.java
@@ -0,0 +1,56 @@
+import java.util.*;
+
+public class EfficientMarkov extends BaseMarkov{
+	
+	HashMap<String, ArrayList<String>> myMap;
+	
+	
+	//initialize myMap and set order
+	public EfficientMarkov(int o) {
+		super(o);
+		myMap = new HashMap<String, ArrayList<String>>();
+	}
+	
+	@Override
+	//method to train the markov machine where k is myMap's key and n is the string following myWords, method maps then updates using an Arraylist with the next strings
+	public void setTraining (String t) {
+		
+		int a = getOrder();
+		myMap.clear();															
+		myText = t;
+		
+		for (int i = 0; (i+a) < t.length(); i ++) {
+			String k= "";
+			String n= "";
+			k= k+ t.substring(i,i+a);
+			
+			if ((t.length() - 1) == (i+a)) 
+				n = PSEUDO_EOS;
+			else 
+				n = t.substring(i+a,1+a+i);
+			
+			if (! myMap.containsKey(k)) {
+				ArrayList<String> follow = new ArrayList<String>();
+				follow.add(n);
+				myMap.put(k, follow);
+			} else {
+				ArrayList<String> value = myMap.get(k);
+				value.add(n);
+				myMap.remove(k);
+				myMap.put(k, value);
+			}
+		}
+	}
+	
+	@Override
+	//returns the possible next strings in an arraylist
+	public ArrayList<String> getFollows (String s) {
+		
+		try {
+			return myMap.get(s);
+		} catch(NoSuchElementException e) {	
+			System.out.println(e.getMessage());
+			return null;
+		}
+	}
+}
---
+++ src/EfficientWordMarkov.java
@@ -0,0 +1,49 @@
+import java.util.*;
+
+public class EfficientWordMarkov extends BaseWordMarkov {
+	
+	HashMap<WordGram, ArrayList<String>> myMap;
+	
+	//initialize mymap and set order
+	public EfficientWordMarkov(int o) {
+		super(o);
+		myMap = new HashMap<WordGram, ArrayList<String>>();
+	}
+	
+	@Override
+	//trains markov machine where a is the wordgram that is the key and n is the string following mywords, method maps inital map then updates using an arraylist with the next strings
+	public void setTraining(String t) {	
+		int s= getOrder();
+		myMap.clear();
+		myWords = t.split("\\s+");
+		for (int i = 0; (i+s) < myWords.length;i++) {
+			WordGram a= new WordGram(myWords,i,s);
+			String n= "";
+			if ((i+s)==(myWords.length-1)) 
+				n= PSEUDO_EOS;
+			 else 
+				n= myWords[i+s];
+			
+			if (! myMap.containsKey(a)) {
+				ArrayList<String> b= new ArrayList<String>();
+				b.add(n);
+				myMap.put(a,b);
+			} else {
+				ArrayList<String> x= new ArrayList<String>(myMap.get(a));
+				x.add(n);
+				myMap.put(a,x);
+			}
+		}
+	}
+	
+	@Override
+	//returns next possible strings in an arraylist
+	public ArrayList<String> getFollows(WordGram wg) {
+		try {
+			return myMap.get(wg);
+		} catch(NoSuchElementException e) {
+			System.out.println(e.getMessage());
+			return null;
+		}
+	}
+}
